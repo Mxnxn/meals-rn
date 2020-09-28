e6f9c08dc3c5a1c7b6a87d9f25a2eab47f5a176b
Manan Soni <manan@xcitech.in>
2020-09-28 16:32:09 +0530

    - fresh.

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import { enableScreens } from "react-native-screens";
+import { createStore, combineReducers } from "redux";
+import mealsReducer from "./store/reducers/meals";
 
+const rootReducer = combineReducers({
+	meals: mealsReducers,
+});
+const store = createStore(rootReducer);
+
+enableScreens();
+const fetchFonts = () => {
+	return Font.loadAsync({
+		GEB: require("./assets/fonts/GEB.otf"),
+		GL: require("./assets/fonts/GL.otf"),
+
+		AB: require("./assets/fonts/AB.ttf"),
+		PR: require("./assets/fonts/PR.ttf"),
+	});
+};
+import MealsNavigator from "./navigations/MealsNavigation";
+import mealsReducers from "./store/reducers/meals";
+import { Provider } from "react-redux";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	const [fontLoaded, setFont] = useState(false);
+
+	if (!fontLoaded) {
+		return (
+			<AppLoading startAsync={fetchFonts} onFinish={() => setFont(true)} />
+		);
+	}
+
+	return (
+		<Provider store={store}>
+			<MealsNavigator />
+		</Provider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
